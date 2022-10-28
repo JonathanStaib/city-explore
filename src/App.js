622dc89330883125c7c4eb60d5bfdfa0acb2bdf1
JonathanStaib <JonathanStaibb@gmail.com>
2022-10-28 00:48:44 -0400

    almost done with lab 08

--- src/App.js
+++ src/App.js
@@ -2,142 +2,161 @@ import './App.css';
 import React from 'react';
 import axios from 'axios';
 import Weather from './Weather';
+import Movie from './Movie';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       cityData: [],
       city: '',
       error: false,
-      errorMessage:'',
+      errorMessage: '',
       lat: '',
-      lon:'',
+      lon: '',
       weatherData: [],
+      movieInfo: [],
 
     }
   }
 
-// handleGetCity = (e)=>{
-//   e.preventDefault();
-//   let cityData = await axios.get();
+  // handleGetCity = (e)=>{
+  //   e.preventDefault();
+  //   let cityData = await axios.get();
 
-//   console.log(cityData.data);
-//   this.setState({
-//     citynData: cityData.data.results,
+  //   console.log(cityData.data);
+  //   this.setState({
+  //     citynData: cityData.data.results,
 
-//   })
-// }
+  //   })
+  // }
 
-handleInput = (e)=> {
-  e.preventDefault();
-  this.setState({
-    city: e.target.value
-  })
-}
-
-handleSubmit = async (e) =>{
-  e.preventDefault();
-}
-
-getCityData = async (e) =>{
-  e.preventDefault();
-
-  try{
-    let locationUrl = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
-  
-    let cityData = await axios.get(locationUrl);
-    let cityToDisplay = cityData.data[0];
-    let lon = cityData.data[0].lon;
-    let lat = cityData.data[0].lat;
-    
-    console.log(cityData.data[0]);
-
-    
+  handleInput = (e) => {
+    e.preventDefault();
     this.setState({
-      cityData: cityToDisplay,
-      error: false,
-      lon: lon,
-      lat: lat,
-    })
-    this.getWeatherData(cityToDisplay);
-    
-  } catch(error){
-    console.log(error);
-    this.setState({
-      error:true,
-      errorMessage: "That is not a Location",
-
+      city: e.target.value
     })
   }
 
-}
-
-getMapData = async (e)=>{
-  e.preventDefault();
-
-  // let Url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=10`
-}
-getWeatherData= async (location)=> {
-  try{
-    let weatherUrl = `${process.env.REACT_APP_SERVER}/key?cityName=${this.state.city}&lat=${location.lat}&lon=${location.lon}`;
-    console.log(weatherUrl);
-    let weatherData = await axios.get(weatherUrl);
-    console.log(weatherData);
-    
-    this.setState({
-      weatherData: weatherData.data
-    });
-    console.log(weatherData);
-  } catch(error){
-    this.setState({
-      error: true,
-      errorMessage: error.message
-    });
+  handleSubmit = async (e) => {
+    e.preventDefault();
   }
-}
 
-render (){
-  
-  return(
+  getCityData = async (e) => {
+    e.preventDefault();
+
+    try {
+      let locationUrl = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+
+      let cityData = await axios.get(locationUrl);
+      let cityToDisplay = cityData.data[0];
+      let lon = cityData.data[0].lon;
+      let lat = cityData.data[0].lat;
+
+      console.log(cityData.data[0]);
+
+
+      this.setState({
+        cityData: cityToDisplay,
+        error: false,
+        lon: lon,
+        lat: lat,
+      })
+      this.getWeatherData(cityToDisplay);
+      this.getMovieData(cityToDisplay);
+
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        error: true,
+        errorMessage: "That is not a Location",
+
+      })
+    }
+
+  }
+
+  getMapData = async (e) => {
+    e.preventDefault();
+
+    // let Url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=10`
+  }
+  getWeatherData = async (location) => {
+    try {
+      let weatherUrl = `${process.env.REACT_APP_SERVER}/weather?lat=${location.lat}&lon=${location.lon}`;
+      // console.log(weatherUrl);
+      let weatherData = await axios.get(weatherUrl);
+      console.log(weatherData);
+
+      this.setState({
+        weatherData: weatherData.data
+      });
+      console.log(weatherData);
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      });
+    }
+  }
+
+  getMovieData = async (movie) => {
+    try {
+      let movieUrl = await axios.get(`${process.env.REACT_APP_SERVER}/movies?city_name=${movie.city}`);
+      this.setState({
+        movieInfo: movieUrl.data
+      });
+      console.log(movieUrl);
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      });
+    }
+  }
+
+  render() {
+
+    return (
     <>
-    <h1>Find A City</h1>
-    
-      {/* {CityItems} */}
+        <h1>Find A City</h1>
 
-      <form onSubmit={this.getCityData}>
-        <label>
-          <input type="text" onInput={this.handleInput}/>
-          <button type='submit'>Explore!</button>
-        </label>
-    </form>
-    
-    {
-      this.state.error ? 
-      <p>{this.state.errorMessage}</p>
-      :
-      <>
-      <ul>
-        <li>
-        City: {this.state.cityData.display_name}
-        </li>
-        <li>
-        Latitude: {this.state.cityData.lat}
-        </li>
-        <li>
-        Longitude: {this.state.cityData.lon}
-        </li>
-      </ul>
-      <section>
-      <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=10`} alt={this.state.cityData.display_name}/>
-      </section>
-      <Weather
-        weatherData={this.state.weatherData}
-      />
-      </>
+        <form onSubmit={this.getCityData}>
+          <label>
+            <input type="text" onInput={this.handleInput} />
+            <button type='submit'>Explore!</button>
+          </label>
+        </form>
+
+        {
+          this.state.error ?
+            <p>{this.state.errorMessage}</p>
+            :
+            <>
+              <ul>
+                <li>
+                  City: {this.state.cityData.display_name}
+                </li>
+                <li>
+                  Latitude: {this.state.cityData.lat}
+                </li>
+                <li>
+                  Longitude: {this.state.cityData.lon}
+                </li>
+              </ul>
+              <section>
+                <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=10`} alt={this.state.cityData.display_name} />
+              </section>
+              <Weather
+                weatherData={this.state.weatherData}
+              />
+              <Movie
+                movieInfo={this.state.movieInfo}
+              />
+              </>
     }
-    </>
+            </>
   )
 }
 }
-export default App;
+        export default App;
---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// Help from https://medium.com/@gpj/making-api-calls-with-react-2e2378acb3ed
+
+
+class Movie extends React.Component {
+  render() {
+    return (
+      <div>
+        <center><h1>Films</h1></center>
+    {this.props.movieInfo.map((film, index) => (
+      <div class="card-body">
+        <img variant="top" src={`https://image.tmdb.org/t/original/${film.img}`} alt="{film.title}"/>
+        <h2 class="card-title">Title: {film.title}</h2>
+        <p class="card-text">Overview: {film.overview}</p>
+        <p class="card-text2">Popularity: {film.popularity}</p>
+        <p class="card-text3">Release Date: {film.release_date}</p>
+          </div>     
+      ))}
+      </div>
+    );
+}
+}
+
+export default Movie;
